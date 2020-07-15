f2263c62c73b387e7e2e0a0e91a843f7f6e7901b
Sabina Acharya <acharya.sabina@gmail.com>
2020-07-15 18:32:23 +0545

    commit2

--- src/main.js
+++ src/main.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { initAppCore } from './core/app';
 
 Vue.config.productionTip = false
+initAppCore();
 
+// create new app v1.0.0
+// this is another project v2.0.0
+// this is a testing 
+// this is a final deployement testing
 new Vue({
   router,
-  store,
+  store,  
   render: h => h(App)
 }).$mount('#app')
