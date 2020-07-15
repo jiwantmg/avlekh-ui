042a938a4f9e5b0ffa161c6abb7a900aa0ba1762
Sabina Acharya <acharya.sabina@gmail.com>
2020-07-15 18:37:25 +0545

    commit4

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <!-- This for commit4 -->
     </div>
     <router-view/>
   </div>
