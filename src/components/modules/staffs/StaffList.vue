65d3b9c5b0b7391c66038066713f86a02d8e186c
Mohan <nahomnep@yahoo.com>
2020-08-09 10:48:29 +0900

    staff register form updated

--- src/components/modules/staffs/StaffList.vue
+++ src/components/modules/staffs/StaffList.vue
@@ -1,15 +1,13 @@
 <template>
   <div>
-      staff view
+      <h5>Staff List</h5>
+      <slot />
   </div>
 </template>
 
 <script>
-export default {
 
-}
 </script>
 
 <style>
-
-</style>
+</style> 
---
+++ src/views/staffs/all.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <StaffList /> 
+  </div>
+</template>
+
+<script>
+ import StaffList from '@/components/modules/staffs/StaffList';
+
+export default {
+  components : {
+    StaffList
+  }
+}
+</script>
+
+<style>
+
+</style>
