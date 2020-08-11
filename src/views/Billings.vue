38331b0191380026164b6c9df723a4c5b5f334f1
min <rajkharel17@gmail.com>
2020-08-11 14:45:32 +0900

    jpt

--- src/views/Billings.vue
+++ src/views/Billings.vue
@@ -1,27 +1,6 @@
 <template>
-  <div>
-      Billing
-  
-<div class = "border p-2">
-<h5 class="font-weight-bold">Billing</h5>
-<div class = "border p-2">
-<form>
-  <div class="form-group">
-  
-    <label class="font-weight-bold" for="email">Email</label>
-    <input type="email"  class="form-control" id="email" >
-   
-  </div>
-  <div class="form-group">
-    <label class = "font-weight-bold" for="phone">Phone</label>
-    <input type="number"  class="form-control" id="phone" >
-
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-  </form>
-</div>
-  
-</div>
+  <div class="font-weight:bold">
+     customers
 </div>
 </template>
 
@@ -36,9 +15,7 @@ export default {
 </script>
 
 <style>
-.form-group .form-control{
-  width: 40%;
-}
+
 
 
 </style>
